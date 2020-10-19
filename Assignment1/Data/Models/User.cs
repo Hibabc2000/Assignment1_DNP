765e27a2986261f8f30a66dcd959204896d88b38
Kristóf Lénárd <293110@via.dk>
2020-10-19 17:49:51 +0200

    Add project files.

---
+++ Assignment1/Data/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Data.Models
+{
+    public class User
+    {
+        private string username;
+        private string password;
+        private ArrayList authority = new ArrayList();
+
+        public User(string username, string password, ArrayList authority)
+        {
+            this.username = username;
+            this.password = password;
+            this.authority = authority;
+        }
+
+        public bool getAuthority(string auth)
+        {
+            if (authority.Contains(auth))
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
